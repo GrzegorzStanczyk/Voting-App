bcec2bfb0c6824167836c7594372db8d9661b412
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-20 23:01:12 +0200

    Connect to herocu via cli

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => console.log(`Server is up on port ${port}`));
