8472fa1cfd8b0a0471f2626b1a06616d91213cee
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-23 00:04:44 +0200

    Added router middleware

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const appRoutes = require('./routes');
+
 const port = process.env.PORT || 3000;
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
+app.use('/', appRoutes);
+
+app.use((req, res) => res.render('index'));
+
+// app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 
 // Start the app by listening on the default Heroku port
 app.listen(port, () => console.log(`Server is up on port ${port}`));
