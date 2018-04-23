be5a135aea69ae5dbfdc280b1c39ea5775c62ab8
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-23 23:33:36 +0200

    Fixed server

--- server.js
+++ server.js
@@ -3,18 +3,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const bodyParser = require('body-parser');
 
 const appRoutes = require('./routes');
 
 const port = process.env.PORT || 3000;
 
+// app.set('view engine', 'html');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 
 app.use('/', appRoutes);
 
-app.use((req, res) => res.render('index'));
+// app.use((req, res) => res.render('index'));
+app.use((req, res) => res.sendFile('index.html'));
 
 // app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 
