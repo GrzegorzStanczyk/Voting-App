9cb892b1a6f3e89f8a12821cec89fab9be2cfc8d
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-25 00:42:31 +0200

    Added vote poll

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { ResultComponent } from './vote/result/result.component';
 import { FeedbackComponent } from './vote/feedback/feedback.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserPollsComponent } from './vote/user-polls/user-polls.component';
+import { VoteComponent } from './vote/vote.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'vote', component: VoteComponent },
   { path: 'feedback', component: FeedbackComponent },
   { path: 'poll', component: PollComponent },
   { path: 'result', component: ResultComponent },
