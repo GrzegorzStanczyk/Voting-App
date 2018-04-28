e762546e29a8efcfd184db5ceb0944dac42fa9eb
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-28 14:50:30 +0200

    Added chart fill delay

--- src/app/main-content/result/result.component.ts
+++ src/app/main-content/result/result.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 
 import { Poll, AppState } from '../../app.component.rx';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-result',
@@ -11,12 +12,14 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ResultComponent implements OnInit {
   result$: Observable<Poll>;
+  delayFlag: boolean = false;
 
   constructor(private store: Store<AppState>) {
     this.result$ = store.pipe(select(state => state.voteApp.poll));
   }
 
   ngOnInit() {
+    this.result$.pipe(first()).subscribe(r => setTimeout(() => this.delayFlag = true));
   }
 
 }
