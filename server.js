a72acd48a1370f7f026d532b11a5a34a6d9b1154
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-29 22:49:06 +0200

    Added socket io module

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
-
-//Install express server
 const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const server = require('http').Server(app);
 
+const io = require('./sockets').init(server);
 const appRoutes = require('./routes');
 
 const port = process.env.PORT || 3000;
 
-// app.set('view engine', 'html');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 
 app.use('/', appRoutes);
-
 // app.use((req, res) => res.render('index'));
-app.use((req, res) => res.sendFile(__dirname, 'index.html'));
+app.use((req, res) => res.sendFile('index.html'));
 
 // app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
 
 // Start the app by listening on the default Heroku port
-app.listen(port, () => console.log(`Server is up on port ${port}`));
+server.listen(port, () => console.log(`Server is up on port ${port}`));
---
+++ sockets/index.js
@@ -0,0 +1,20 @@
+const sio = require('socket.io');
+let io = null;
+
+exports.io = () => io;
+
+exports.init = server => {
+  io = sio(server);
+
+  io.on('connection', socket => {
+    socket.emit('news', { hello: 'world'});
+    socket.on('event', data => {
+      console.log(data);
+      // socket.broadcast.emit('poll', { poll: 'poll' });
+      io.sockets.emit('poll', { poll: 'poll' });
+    });
+  })
+}
+
+// For use in consumers
+// const socket = require('../io').io();
