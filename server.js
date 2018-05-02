e3f50adc1176bdf1a0514c0bebabb0c7ea49ca08
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-05-02 12:23:15 +0200

    Added router

--- server.js
+++ server.js
@@ -8,19 +8,13 @@ const initializeDatabases = require('./dbs');
 
 const appRoutes = require('./routes');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
-// app.set('view engine', 'html');
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 
-app.use('/', appRoutes);
-
-// app.use((req, res) => res.render('index'));
-app.use((req, res) => res.sendFile(__dirname, 'index.html'));
-
-// app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/dist/index.html')));
+app.use(appRoutes);
 
 // Start the app by listening on the default Heroku port
 app.listen(port, () => console.log(`Server is up on port ${port}`));
